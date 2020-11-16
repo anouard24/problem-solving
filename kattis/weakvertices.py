8f3e3551bf9080f8e7305a09519fe7c638c42021
Anouar Fadili (anouard24) <anouar.fadili.3@gmail.com>
2020-11-16 22:04:46 +0100

    Refactor weakvertices problem code

--- kattis/weakvertices.py
+++ kattis/weakvertices.py
@@ -12,18 +12,16 @@ class UnionFind:
         self.ids[p] = p
 
     def contains(self, p):
-        return self.ids.get(p, None) != None
+        return bool(self.ids.get(p))
 
     def find(self, p):
         r = p
         while r != self.ids[r]:
             r = self.ids[r]
-
         while p != r:
             pp = self.ids[p]
             self.ids[p] = r
             p = pp
-
         return r
 
     def connected(self, p, q):
@@ -32,36 +30,18 @@ class UnionFind:
     def componentsSize(self, p):
         return self.szs[self.find(p)]
 
-    def size(self):
-        return self.size
-
-    def components(self):
-        return self.numComponents
-
     def unify(self, p, q):
         root1 = self.find(p)
         root2 = self.find(q)
         if root1 == root2:
             return
-
         if self.szs[root1] < self.szs[root2]:
             self.szs[root2] = self.szs[root2] + self.szs[root1]
             self.ids[root1] = root2
         else:
             self.szs[root1] = self.szs[root1] + self.szs[root2]
             self.ids[root2] = root1
-
         self.numComponents -= 1
-
-    def minmax(self):
-        mini = self.size
-        maxi = 0
-        for i in self.ids:
-            if i == self.ids[i]:
-                mini = min(mini, self.szs[i])
-                maxi = max(maxi, self.szs[i])
-
-        return mini, maxi
 
 
 # main program
